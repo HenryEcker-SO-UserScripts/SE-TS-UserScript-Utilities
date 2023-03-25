edd49fe28c01e040177431725343e90bca394c4c
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-03-24 22:13:28 -0400

    Support new plagiarism endpoints

--- Utilities/PostFlagsAndVotes.ts
+++ Utilities/PostFlagsAndVotes.ts
@@ -18,3 +18,41 @@ export function reopenQuestion(postId: IdType) {
         }
     );
 }
+
+
+interface FlagPlagiarismResponse {
+    FlagType: number;
+    Message: string;
+    Outcome: number;
+    ResultChangedState: boolean;
+    Success: boolean;
+}
+
+export function flagPlagiarizedContent(postId: IdType, originalSource: string, detailText: string) {
+    return fetchPostFormDataBodyJsonResponse<FlagPlagiarismResponse>(
+        `/flags/posts/${postId}/add/PlagiarizedContent`,
+        {
+            fkey: StackExchange.options.user.fkey,
+            otherText: detailText,
+            customData: JSON.stringify({plagiarizedSource: originalSource}),
+            overrideWarning: false
+        }
+    );
+}
+
+
+// Currently relying on 200 status code since it sometimes returns an HTML response and sometimes JSON
+// Depending on if the CM escalate message was displayed or not.
+// export interface PostDeleteAsPlagiarismResponse {
+//     success: boolean;
+//     message: string;
+// }
+
+export function deleteAsPlagiarism(postId: IdType) {
+    return fetchPostFormData(
+        `/admin/posts/${postId}/delete-as-plagiarism`,
+        {
+            fkey: StackExchange.options.user.fkey
+        }
+    )
+}
