c0b0a7b888ffecd3b8edf7ef649993420dee729b
HenryEcker <HenryEcker@users.noreply.github.com>
2023-04-02 01:48:56 -0400

    Add char counter helper function to toolkit

---
+++ StacksHelpers/StacksCharCounter.ts
@@ -0,0 +1,27 @@
+export interface CharCounterOptions {
+    min: number;
+    max: number;
+    setIsValid?: (b: boolean) => void;
+    target?: JQuery | HTMLElement;
+    startAt?: number;
+}
+
+declare global {
+    interface JQuery {
+        charCounter: (options: CharCounterOptions) => JQuery;
+    }
+}
+
+export function configureCharCounter(
+    jTextarea: JQuery<HTMLTextAreaElement>,
+    populateText: string,
+    charCounterOptions: CharCounterOptions
+) {
+    if (charCounterOptions.target === undefined) {
+        charCounterOptions.target = jTextarea.parent().find('span.text-counter');
+    }
+    jTextarea
+        .val(populateText)
+        .charCounter(charCounterOptions)
+        .trigger('charCounterUpdate');
+}
---
+++ cjs/StacksHelpers/StacksCharCounter.d.ts
@@ -0,0 +1,13 @@
+export interface CharCounterOptions {
+    min: number;
+    max: number;
+    setIsValid?: (b: boolean) => void;
+    target?: JQuery | HTMLElement;
+    startAt?: number;
+}
+declare global {
+    interface JQuery {
+        charCounter: (options: CharCounterOptions) => JQuery;
+    }
+}
+export declare function configureCharCounter(jTextarea: JQuery<HTMLTextAreaElement>, populateText: string, charCounterOptions: CharCounterOptions): void;
