2d4b8c8c43bdfebb17077dc38762d17f6df02bae
HenryEcker <hde1997@gmail.com>
2023-04-02 01:48:56 -0400

    Add char counter helper function to toolkit

---
+++ cjs/StacksHelpers/StacksCharCounter.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.configureCharCounter = void 0;
+function configureCharCounter(jTextarea, populateText, charCounterOptions) {
+    if (charCounterOptions.target === undefined) {
+        charCounterOptions.target = jTextarea.parent().find('span.text-counter');
+    }
+    jTextarea
+        .val(populateText)
+        .charCounter(charCounterOptions)
+        .trigger('charCounterUpdate');
+}
+exports.configureCharCounter = configureCharCounter;
+//# sourceMappingURL=StacksCharCounter.js.map
