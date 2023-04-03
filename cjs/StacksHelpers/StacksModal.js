f64811a022b1d17acaaf568ef18780e4f51bd133
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-04-02 21:34:47 -0400

    Add submit handle helper

--- cjs/StacksHelpers/StacksModal.js
+++ cjs/StacksHelpers/StacksModal.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.removeModalFromDOM = void 0;
+exports.disableSubmitButtonAndToastErrors = exports.removeModalFromDOM = void 0;
+const ErrorHandling_1 = require("../Utilities/ErrorHandling");
 function removeModalFromDOM(modalId) {
     const existingModal = document.getElementById(modalId);
     if (existingModal !== null) {
@@ -11,4 +12,21 @@ function removeModalFromDOM(modalId) {
     }
 }
 exports.removeModalFromDOM = removeModalFromDOM;
+async function disableSubmitButtonAndToastErrors(jSubmitButton, handleActions) {
+    jSubmitButton
+        .prop('disabled', true)
+        .addClass('is-loading');
+    try {
+        await handleActions();
+    }
+    catch (error) {
+        StackExchange.helpers.showToast((0, ErrorHandling_1.getMessageFromCaughtElement)(error), { type: 'danger' });
+    }
+    finally {
+        jSubmitButton
+            .prop('disabled', false)
+            .removeClass('is-loading');
+    }
+}
+exports.disableSubmitButtonAndToastErrors = disableSubmitButtonAndToastErrors;
 //# sourceMappingURL=StacksModal.js.map
