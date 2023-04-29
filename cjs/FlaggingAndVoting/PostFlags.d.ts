2b719b6e5d12cbae4dae31db857ba32d57d1686d
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-04-28 22:15:44 -0400

    Separate questionFlags into own file; add close as duplicate function

--- cjs/FlaggingAndVoting/PostFlags.d.ts
+++ cjs/FlaggingAndVoting/PostFlags.d.ts
@@ -1,8 +1,6 @@
 import { type IdType } from '../Utilities/Types';
-import { type FlagResponse } from './Helpers';
-export declare function reopenQuestion(postId: IdType): Promise<FlagResponse>;
-export declare function flagSpam(postId: IdType): Promise<FlagResponse>;
-export declare function flagRudeOrAbusive(postId: IdType): Promise<FlagResponse>;
-export declare function flagPlagiarizedContent(postId: IdType, originalSource: string, detailText: string): Promise<FlagResponse>;
-export declare function flagVeryLowQuality(postId: IdType): Promise<FlagResponse>;
-export declare function flagInNeedOfModeratorIntervention(postId: IdType, detailText: string): Promise<FlagResponse>;
+export declare function flagSpam(postId: IdType): Promise<import("./Helpers").FlagResponse>;
+export declare function flagRudeOrAbusive(postId: IdType): Promise<import("./Helpers").FlagResponse>;
+export declare function flagPlagiarizedContent(postId: IdType, originalSource: string, detailText: string): Promise<import("./Helpers").FlagResponse>;
+export declare function flagVeryLowQuality(postId: IdType): Promise<import("./Helpers").FlagResponse>;
+export declare function flagInNeedOfModeratorIntervention(postId: IdType, detailText: string): Promise<import("./Helpers").FlagResponse>;
--- cjs/FlaggingAndVoting/PostFlags.js
+++ cjs/FlaggingAndVoting/PostFlags.js
@@ -1,14 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.flagInNeedOfModeratorIntervention = exports.flagVeryLowQuality = exports.flagPlagiarizedContent = exports.flagRudeOrAbusive = exports.flagSpam = exports.reopenQuestion = void 0;
+exports.flagInNeedOfModeratorIntervention = exports.flagVeryLowQuality = exports.flagPlagiarizedContent = exports.flagRudeOrAbusive = exports.flagSpam = void 0;
 const Helpers_1 = require("./Helpers");
-const General_1 = require("../Utilities/General");
-function reopenQuestion(postId) {
-    return (0, General_1.ajaxPostWithData)(`/flags/questions/${postId}/reopen/add`, {
-        fkey: StackExchange.options.user.fkey
-    });
-}
-exports.reopenQuestion = reopenQuestion;
 function flagSpam(postId) {
     return (0, Helpers_1.flagPost)('PostSpam', postId, null, true);
 }
