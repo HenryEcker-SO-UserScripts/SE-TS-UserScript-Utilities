d1c24369eea3628006bfe61512d6e8ad4c760d55
Henry Ecker <HenryEcker@users.noreply.github.com>
2023-05-07 21:40:38 -0400

    Add ajaxGet and fetch vote counts endpoint

--- cjs/FlaggingAndVoting/PostVotes.d.ts
+++ cjs/FlaggingAndVoting/PostVotes.d.ts
@@ -1,3 +1,7 @@
 import { type IdType } from '../Utilities/Types';
 export declare function deletePost(postId: IdType): Promise<import("./Helpers").VoteResponse>;
 export declare function undeletePost(postId: IdType): Promise<import("./Helpers").VoteResponse>;
+export declare function getVoteCounts(postId: IdType): Promise<{
+    down: string;
+    up: string;
+}>;
