7e691781bb0f1bc1765a21464ee9d2b4068ab679
HenryEcker <HenryEcker@users.noreply.github.com>
2023-09-17 21:16:32 -0400

    Build on Stacks Controller Definition for better compatibility

--- Utilities/Types.ts
+++ Utilities/Types.ts
@@ -1,6 +1,8 @@
+import {type ControllerDefinition} from '@stackoverflow/stacks';
+
 export type IdType = number | string;
 
-export interface BaseStacksControllerConfig {
+export interface BaseStacksControllerConfig extends ControllerDefinition {
     targets?: string[];
     initialize?: () => void;
     connect?: () => void;
