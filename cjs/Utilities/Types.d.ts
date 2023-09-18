16ad87cf380f8ebb0d015db6dcc0e431053b5acd
HenryEcker <HenryEcker@users.noreply.github.com>
2023-09-17 21:16:55 -0400

    Rebuild

--- cjs/Utilities/Types.d.ts
+++ cjs/Utilities/Types.d.ts
@@ -1,5 +1,6 @@
+import { type ControllerDefinition } from '@stackoverflow/stacks';
 export type IdType = number | string;
-export interface BaseStacksControllerConfig {
+export interface BaseStacksControllerConfig extends ControllerDefinition {
     targets?: string[];
     initialize?: () => void;
     connect?: () => void;
